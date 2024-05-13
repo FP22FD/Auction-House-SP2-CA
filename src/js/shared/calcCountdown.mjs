94efcf3d1010fa9fd42142de7933891065aed50e
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-05-13 16:07:36 +0200

    Implement function countDown for the listings/cards

---
+++ src/js/shared/calcCountdown.mjs
@@ -0,0 +1,49 @@
+// Ref: https://www.w3schools.com/howto/howto_js_countdown.asp
+export function countDown(intervalMs, date, callback) {
+  const countDownDate = date.getTime();
+
+  // Ref: How to tick immediately: https://stackoverflow.com/a/20706004
+  setTimeout(() => {
+    const now = new Date().getTime();
+    const { distance, days, hours, minutes, seconds } = calcCountdown(
+      now,
+      countDownDate,
+    );
+    callback({ distance, days, hours, minutes, seconds });
+  }, 0);
+
+  const x = setInterval(function () {
+    const now = new Date().getTime();
+
+    const { distance, days, hours, minutes, seconds } = calcCountdown(
+      now,
+      countDownDate,
+    );
+
+    callback({ distance, days, hours, minutes, seconds });
+
+    if (distance < 0) {
+      clearInterval(x);
+    }
+  }, intervalMs);
+}
+
+// TODO: make unit test
+export function calcCountdown(start, end) {
+  const distance = end - start;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  return {
+    distance,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
